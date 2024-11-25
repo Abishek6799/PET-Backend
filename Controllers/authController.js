1361e37371869329a6fb76a85114be4242411cd2
Abishek6799 <abiabishek310@gmail.com>
2024-11-25 06:09:28 +0530

    update

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -115,7 +115,7 @@ export const forgotPassword = async (req, res) => {
             return res.status(400).json({ message: "User Not Found" });
         }
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
-        const resetPasswordLink = `http://localhost:5173/reset-password/${user._id}/${token}`;
+        const resetPasswordLink = `https://imaginative-bublanina-967d54.netlify.app/reset-password/${user._id}/${token}`;
         const emailSend = await sendMail({
             to:user.email,
             subject:'Reset Password',
